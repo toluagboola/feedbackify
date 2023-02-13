59a962e666ae2398affede1b2bd904c7cf95d490
toluagboola <toluwanimiagboola@gmail.com>
2023-02-13 12:43:17 +0100

    Create add and delete feedback functionality

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { FeedbackStore } from "../stores";
+  import { scale, fade } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade={{ duration: 300 }}>
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
