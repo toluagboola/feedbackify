09ba6847e25a9c8230592d36fa3f3c9287dd0452
toluagboola <toluwanimiagboola@gmail.com>
2023-02-15 15:20:52 +0100

    Add header and footer with links

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,26 @@
   import FeedbackForm from "./components/FeedbackForm.svelte";
   import FeedbackList from "./components/FeedbackList.svelte";
   import FeedbackStats from "./components/FeedbackStats.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <main class="container">
+  <h1><a href="/">Feedbackify</a></h1>
   <FeedbackForm />
   <FeedbackStats />
   <FeedbackList />
+  <Footer />
 </main>
+
+<style>
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+    padding-bottom: 40px;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,31 @@
+<div class="flex">
+  <p>
+    Built with <a href="https://svelte.dev/">Svelte</a> by
+    <a href="https://toluagboola.github.io/"
+      ><strong>Toluwanimi Isaiah</strong></a
+    >
+  </p>
+
+  <div>
+    <ul>
+      <li><a href="https://github.com/toluagboola/feedbackify">GitHub</a></li>
+    </ul>
+  </div>
+</div>
+
+<style>
+  .flex {
+    display: flex;
+    padding-top: 40px;
+    justify-content: space-between;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
